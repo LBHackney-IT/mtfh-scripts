c1451166f18899f65e0807f19f848b8fc6e20bc5
Adam Tracy <adam.tracy20@gmail.com>
2023-04-04 11:32:26 +0100

    Updated imports

--- aws/src/database/dynamodb/utils/dynamodb_item_factory.py
+++ aws/src/database/dynamodb/utils/dynamodb_item_factory.py
@@ -2,11 +2,10 @@ from typing import Any, Callable
 
 import dacite.exceptions
 from dacite import from_dict
-
-from src.utils.filter_list_of_dictionaries_by_lambdas import filter_list_of_dictionaries_by_lambdas
-from src.utils.logger import Logger
-
 from mypy_boto3_dynamodb.service_resource import Table
+
+from aws.src.utils.filter_list_of_dictionaries_by_lambdas import filter_list_of_dictionaries_by_lambdas
+from aws.src.utils.logger import Logger
 
 
 class DynamodbItemFactory:
@@ -21,10 +20,10 @@ class DynamodbItemFactory:
             headings_filters = {"id": lambda x: bool(x)}
         scan = self.table.scan()
         raw_items += filter_list_of_dictionaries_by_lambdas(scan["Items"], headings_filters)
-        while "LastEvaluatedKey" in scan:
-            self.logger and self.logger.log(f"Scanned {len(scan)} items - last key: {scan['LastEvaluatedKey']}")
-            scan = self.table.scan(ExclusiveStartKey=scan["LastEvaluatedKey"])
-            raw_items += filter_list_of_dictionaries_by_lambdas(scan["Items"], headings_filters)
+        # while "LastEvaluatedKey" in scan:
+        #     self.logger and self.logger.log(f"Scanned {len(scan)} items - last key: {scan['LastEvaluatedKey']}")
+        #     scan = self.table.scan(ExclusiveStartKey=scan["LastEvaluatedKey"])
+        #     raw_items += filter_list_of_dictionaries_by_lambdas(scan["Items"], headings_filters)
         self.logger and self.logger.log(f"Finished run - scanned {len(scan)} items")
         try:
             items = [from_dict(data_class=self.output_class, data=item) for item in raw_items]
