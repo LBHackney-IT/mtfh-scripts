9b98fb29881b9477455b0f41f5b37e7a5cc1a646
Adam Tracy <adam.tracy20@gmail.com>
2023-04-04 11:25:33 +0100

    Reverted to previous solution Dynamodb to CSV requires more work

--- aws/src/database/dynamodb/scripts/asset_table/get_assets.py
+++ aws/src/database/dynamodb/scripts/asset_table/get_assets.py
@@ -1,7 +1,10 @@
+import csv
 from dataclasses import dataclass
 
 from aws.src.database.dynamodb.domain.domain_objects import Asset
-from aws.src.database.dynamodb.utils.dynamodb_to_csv import dynamodb_to_csv
+from aws.src.database.dynamodb.utils.dynamodb_item_factory import DynamodbItemFactory
+from aws.src.database.dynamodb.utils.get_dynamodb_table import get_dynamodb_table
+from aws.src.utils.logger import Logger
 from enums.enums import Stage
 
 
@@ -9,19 +12,20 @@ from enums.enums import Stage
 class Config:
     TABLE_NAME = "Assets"
     OUTPUT_CLASS = Asset
-    STAGE = Stage.HOUSING_DEVELOPMENT
+    LOGGER = Logger()
+    STAGE = Stage.BASE_DEVELOPMENT
     OUTPUT_DIRECTORY = "../../output"
-    
-    # Put all headings / keys you want to extract from the DynamoDB table here
     HEADING_FILTERS = {
         "id": lambda x: bool(x),
-        "assetId": lambda x: bool(x),
-        "patches": lambda x: bool(x),
     }
+
 
 if __name__ == "__main__":
     with open(Config.OUTPUT_DIRECTORY, "w") as outfile:
         outfile.write("")
+        writer = csv.writer(outfile)
+        table = get_dynamodb_table(Config.TABLE_NAME, Config.STAGE)
+        dynamodb_item_factory = DynamodbItemFactory(table, Config.OUTPUT_CLASS, Config.LOGGER)
+        assets = dynamodb_item_factory.full_extract(Config.HEADING_FILTERS)
 
-    # This works, but doesn't give any fine control, could write more generic scripts
-    dynamodb_to_csv("Assets", Config.STAGE, Config.OUTPUT_DIRECTORY, Config.HEADING_FILTERS)
+        # Script here to parse assets (including nested JSON) and write to csv
