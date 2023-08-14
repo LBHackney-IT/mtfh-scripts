336f44d885881fc3bc93dc9174f4c23bdf49d234
Adam <adam.tracy20@gmail.com>
2023-08-14 10:19:41 +0100

    Update domain objects

--- aws/src/database/domain/dynamo_domain_objects.py
+++ aws/src/database/domain/dynamo_domain_objects.py
@@ -1,7 +1,55 @@
 from _decimal import Decimal
 from dataclasses import dataclass
 
+from aws.src.utils.safe_object_from_dict import safe_object_from_dict
 
+# --- Tenure Table ---
+@dataclass
+class TenuredAsset:
+    id: str
+    fullAddress: str | None
+    propertyReference: str | None
+    uprn: str | None
+    type: str | None
+
+
+@dataclass
+class HouseholdMember:
+    id: str
+    fullName: str | None
+    dateOfBirth: str | None
+    isResponsible: bool | None
+    personTenureType: str | None
+    type: str | None
+
+
+@dataclass
+class Tenure:
+    id: str
+    charges: dict | None
+    endOfTenureDate: str | None
+    evictionDate: str | None
+    householdMembers: list[HouseholdMember] | None
+    informHousingBenefitsForChanges: bool | None
+    isMutalExchange: bool | None
+    isSublet: bool | None
+    legacyReferences: list[dict] | None
+    notices: list[dict] | None
+    paymentReference: str | None
+    potentialEndDate: str | None
+    startOfTenureDate: str | None
+    subletEndDate: str | None
+    successionDate: str | None
+    tenuredAsset: TenuredAsset | None
+    tenureType: dict | None
+    terminated: dict | None
+
+    def __post_init__(self):
+        self.tenuredAsset = safe_object_from_dict(TenuredAsset, self.tenuredAsset)
+        self.householdMembers = [safe_object_from_dict(HouseholdMember, member) for member in self.householdMembers]
+# --- END Tenure Table ---
+
+# --- Person Table ---
 @dataclass
 class PersonTenure:
     id: str
@@ -16,28 +64,6 @@ class PersonTenure:
 
 
 @dataclass
-class Tenure:
-    id: str
-    charges: list[dict] | None
-    endOfTenureDate: str | None
-    evictionDate: str | None
-    householdMembers: list[dict]
-    informHousingBenefitsForChanges: bool | None
-    isMutalExchange: bool | None
-    isSublet: bool | None
-    legacyReferences: list[dict] | None
-    notices: list[dict] | None
-    paymentReference: str | None
-    potentialEndDate: str | None
-    startOfTenureDate: str | None
-    subletEndDate: str | None
-    successionDate: str | None
-    tenuredAsset: dict | None
-    tenureType: dict | None
-    terminated: dict | None
-
-
-@dataclass
 class Person:
     id: str
     title: str | None
@@ -48,7 +74,12 @@ class Person:
     personTypes: list[str]
     tenures: list[PersonTenure]
 
+    def __post_init__(self):
+        self.tenures = [safe_object_from_dict(PersonTenure, tenure) for tenure in self.tenures]
+# --- END Person Table ---
 
+
+# --- Asset Table ---
 @dataclass
 class ResponsibleEntity:
     id: str
@@ -65,6 +96,10 @@ class Patch:
     patchType: str | None
     responsibleEntities: list[ResponsibleEntity]
     versionNumber: Decimal | None
+
+    def __post_init__(self):
+        self.responsibleEntities = [safe_object_from_dict(ResponsibleEntity, entity) for entity in
+                                    self.responsibleEntities]
 
 
 @dataclass
@@ -90,3 +125,8 @@ class Asset:
     rootAsset: str | None
     tenure: AssetTenure | None
     versionNumber: Decimal | None
+
+    def __post_init__(self):
+        self.tenure = safe_object_from_dict(AssetTenure, self.tenure)
+        self.patches = [safe_object_from_dict(Patch, patch) for patch in self.patches]
+# --- END Asset Table ---
