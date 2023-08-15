cd6304497b3ddd131e5c10cadf79e0abe1fef2a3
Adam <adam.tracy20@gmail.com>
2023-08-15 16:51:12 +0100

    Test for handling of undefined fields

--- aws/tests/utils/test_safe_object_from_dict.py
+++ aws/tests/utils/test_safe_object_from_dict.py
@@ -6,7 +6,7 @@ from aws.src.utils.safe_object_from_dict import safe_object_from_dict
 
 def test_safe_object_from_dict_generates_tenure():
     """
-    Test that safe_object_from_dict returns a Tenure object with the correct values
+    Returns a Tenure object with the correct values
     """
     eg_tenure = {
         "id": '4c148ab7-a58b-a4c2-053a-03bc1684b559',
@@ -24,3 +24,33 @@ def test_safe_object_from_dict_generates_tenure():
     # Does inner type conversion
     assert isinstance(tenure.householdMembers[0], HouseholdMember)
     assert tenure.householdMembers[0].fullName == 'FAKE_First FAKE_Last'
+
+def test_safe_object_from_dict_handles_undefined_fields():
+    """
+    Not affected by undefined fields in outer and inner objects and still allows for inner conversion
+    """
+    eg_tenure = {
+        "id": '4c148ab7-a58b-a4c2-053a-03bc1684b559',
+        "undefinedOuterField": "undefined",
+        'householdMembers': [
+            {'fullName': 'FAKE_First FAKE_Last', 'isResponsible': True, 'dateOfBirth': '1900-03-02T00:00:00',
+             'personTenureType': 'Tenant', 'id': '713ac847-99fb-c771-5a72-55542a76dcc2', 'type': 'Person',
+             'undefinedInnerField': 'undefined'
+            }]
+    }
+
+    tenure = safe_object_from_dict(Tenure, eg_tenure)
+
+    # Sets defined fields on outer object
+    assert hasattr(tenure, 'id')
+    assert hasattr(tenure, 'householdMembers')
+
+    # Does not set undefined fields on outer object
+    assert not hasattr(tenure, 'undefinedOuterField')
+
+    # Sets defined fields on inner object
+    assert hasattr(tenure.householdMembers[0], 'id')
+    assert hasattr(tenure.householdMembers[0], 'fullName')
+
+    # Does not set undefined fields on inner object
+    assert not hasattr(tenure.householdMembers[0], 'undefinedInnerField')
