5e506e619363aea33a1c676b80b2ef3ac599287c
Adam <adam.tracy20@gmail.com>
2023-08-17 16:14:41 +0100

    Generalised main

--- gcp/src/service_account/main.py
+++ gcp/src/service_account/main.py
@@ -4,10 +4,9 @@ from gcp.src.service_account.utils.DriveServiceAccountClient import DriveService
 credentials = get_credentials()
 service_account = DriveServiceAccountClient(credentials)
 
-service_account = DriveServiceAccountClient(CREDENTIALS)
 
 
 def main():
     # Replace this and write your code below
-    folder_id = FOLDERS["MY_DRIVE"]
+    folder_id = input("folder to download: ")
     service_account.download_folder_contents(folder_id, "my_drive")
