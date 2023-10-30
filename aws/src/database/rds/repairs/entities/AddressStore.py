ac0e8a2777a3d49baa974cc98cf01c327d266530
Adam <adam.tracy20@gmail.com>
2023-10-30 16:49:26 +0000

    Connection to repairs DB

---
+++ aws/src/database/rds/repairs/entities/AddressStore.py
@@ -0,0 +1,22 @@
+from dataclasses import dataclass, asdict
+
+from sqlalchemy import String
+from sqlalchemy.orm import DeclarativeBase, MappedAsDataclass, Mapped, mapped_column
+
+
+class Base(MappedAsDataclass, DeclarativeBase):
+    pass
+
+
+@dataclass
+class AddressStore(Base):
+    __tablename__ = "address_store"
+    __table_args__ = {"schema": "dbo"}
+
+    address: Mapped[str] = mapped_column(primary_key=True)
+    postcode: Mapped[str] = mapped_column(String(10))
+    
+    to_dict = asdict
+
+    def __repr__(self) -> str:
+        return f"AddressStore(address={self.address!r}, postcode={self.postcode!r})"
