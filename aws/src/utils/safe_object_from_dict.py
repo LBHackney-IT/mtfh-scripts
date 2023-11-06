f53f4628386401b6b3561cfe9ce2294275a58190
Adam <adam.tracy20@gmail.com>
2023-11-06 10:22:08 +0000

    Disabled logs in safe obj from dict
    Co-authored-by: ShaktiBhakar <ShaktiBhakar@users.noreply.github.com>

--- aws/src/utils/safe_object_from_dict.py
+++ aws/src/utils/safe_object_from_dict.py
@@ -16,12 +16,12 @@ def safe_object_from_dict(object_type: type[T], data: dict) -> T:
 
     for key in list(data.keys()):
         if key not in object_type.__annotations__.keys():
-            print(f"WARNING: {key} not in {object_type.__name__}")
+            # print(f"WARNING: {key} not in {object_type.__name__}")
             del data[key]
 
     for key in object_type.__annotations__.keys():
         if key not in data.keys():
-            print(f"WARNING: {key} from {object_type.__name__} not in data")
+            # print(f"WARNING: {key} from {object_type.__name__} not in data")
             data[key] = None
 
     return object_type(**data)
