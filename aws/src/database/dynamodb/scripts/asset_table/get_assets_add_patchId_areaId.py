e1a5983352612567e0b0fc8aa96a0b66a3467eec
Adam <adam.tracy20@gmail.com>
2023-12-06 11:49:27 +0000

    Finalised script

--- aws/src/database/dynamodb/scripts/asset_table/get_assets_add_patchId_areaId.py
+++ aws/src/database/dynamodb/scripts/asset_table/get_assets_add_patchId_areaId.py
@@ -26,21 +26,22 @@ from utils.confirm import confirm
 class Config:
     TABLE_NAME = "Assets"
     LOGGER = Logger()
-    STAGE = Stage.HOUSING_STAGING
+    STAGE = Stage.HOUSING_DEVELOPMENT
     if Path(__file__).parent.name == "mtfh-scripts":
         INPUT_FILE = "aws/src/database/dynamodb/scripts/asset_table/input/assetsStaging.json"
         PROCESSED_IDS_FILE = "aws/src/database/dynamodb/scripts/asset_table/output/processed_ids.csv"
     else:
-        INPUT_FILE = "input/assetsStaging.json"
+        INPUT_FILE = "input/assetsDev.json"
         PROCESSED_IDS_FILE = "output/processed_ids.csv"
-    ASSETS_PER_SECOND = 8
+    ASSETS_PER_SECOND = 8 / 1.5
     LIMIT = False  # Set to False when ready to run on all assets
 
 
 def add_processed_id(asset_pk: str, success: bool, reason: str = ""):
-    print(f"Processed asset {asset_pk}")
+    message = f"{asset_pk}, {success}, {reason}"
+    Config.LOGGER.log(message)
     with open(Config.PROCESSED_IDS_FILE, "a") as f:
-        f.write(f"{asset_pk}, {success}, {reason}\n")
+        f.write("\n" + message)
 
 
 def load_assets(file_path: str) -> list[dict]:
@@ -74,26 +75,19 @@ def change_asset_schema(raw_asset: dict) -> dict | None:
         - Sets patchId and areaId attributes
     """
     if not raw_asset.get("rootAsset"):
-        Config.LOGGER.log(f"Asset has no root asset, {raw_asset['id']}, skipping")
         add_processed_id(raw_asset["id"], False, "Has no root asset - required field")
         return None
     if raw_asset.get("patches") in [None, []]:
-        Config.LOGGER.log(f"Asset has no patches assigned, {raw_asset['id']}, skipping")
-        add_processed_id(raw_asset["id"], False, "No patches assigned")
+        add_processed_id(raw_asset["id"], False, f"No patches assigned: {raw_asset.get('patches')}")
         return None
     try:
-        if len(raw_asset["patches"]) > 1 and Config.STAGE == Stage.HOUSING_PRODUCTION:
-            Config.LOGGER.log(f"Prod Asset has more than one patch assigned to it, {raw_asset['id']}, skipping")
-            return None
-
         patches: list[dict] = [patch for patch in raw_asset["patches"] if patch.get("patchType") == "patch"]
         patch = patches[0] if patches else None
-        if patch:
-            raw_asset["patchId"] = patch["id"] if patch.get("id") else None
-            raw_asset["areaId"] = patch["parentId"] if patch.get("parentId") else None
-        else:
+        if patch is None:
             Config.LOGGER.log(f"Asset has no patch assigned, {raw_asset['id']}, skipping")
             return None
+        raw_asset["patchId"] = patch["id"] if patch.get("id") else None
+        raw_asset["areaId"] = patch["parentId"] if patch.get("parentId") else None
         raw_asset.pop("patches", None)
         return raw_asset
     except Exception as e:
@@ -102,13 +96,12 @@ def change_asset_schema(raw_asset: dict) -> dict | None:
         return None
 
 
-def update_assets(asset_table: Table, updated_assets: list[Asset]) -> int:
+def update_assets(asset_table: Table, updated_assets: list[Asset]):
     """
     update the asset record that are already assigned to a patch to have areaId and patchId and remove patches
     """
     delay_per_asset = round(1 / Config.ASSETS_PER_SECOND, 2)
     print(f"Processing {Config.ASSETS_PER_SECOND} assets per second with a delay of {delay_per_asset} seconds per asset")
-    update_count = 0
     progress_bar = ProgressBar(len(updated_assets))
     for i, asset_item in enumerate(updated_assets):
         if i % 100 == 0:
@@ -116,13 +109,12 @@ def update_assets(asset_table: Table, updated_assets: list[Asset]) -> int:
         try:
             asset_item.versionNumber = asset_item.versionNumber + 1 if asset_item.versionNumber else 0
             asset_table.put_item(Item=asdict(asset_item))
-            update_count += 1
             add_processed_id(asset_item.id, True)
             time.sleep(delay_per_asset)
         except Exception as e:
             Config.LOGGER.log(f"Failed to update asset {asset_item.id} with error {e}")
             add_processed_id(asset_item.id, False, str(e))
-    return update_count
+    return
 
 
 def main():
@@ -133,7 +125,6 @@ def main():
     for asset_datum in asset_data:
         if asset_datum.get("patches") is None and asset_datum.get("patchId") is not None:
             print(f"Asset {asset_datum['id']} has patchId but no patches, skipping")
-            add_processed_id(asset_datum["id"], False)
             continue
         fixed_asset = change_asset_schema(asset_datum)
         if fixed_asset is None:
@@ -142,8 +133,8 @@ def main():
         assets_to_update.append(asset)
 
     if confirm(f"Are you sure you want to update {len(assets_to_update)} assets in {Config.STAGE.to_env_name()}?"):
-        update_count = update_assets(table, assets_to_update)
-        Config.LOGGER.log(f"Updated {update_count} assets out of {len(assets_to_update)}")
+        update_assets(table, assets_to_update)
+        Config.LOGGER.log(f"Updated assets out of {len(assets_to_update)}")
 
 
 if __name__ == "__main__":
