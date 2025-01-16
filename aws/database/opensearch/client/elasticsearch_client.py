78aa54606e70dfefcbbcbf3e540487fe5962465d
Adam <adam.tracy20@gmail.com>
2025-01-16 11:15:16 +0000

    Create script

--- aws/database/opensearch/client/elasticsearch_client.py
+++ aws/database/opensearch/client/elasticsearch_client.py
@@ -6,6 +6,30 @@ import urllib3
 # Suppress warnings about insecure connections - this is because we're connecting to localhost and not using SSL
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
+
+def create_client(index: str | None, port: int = 3333) -> Elasticsearch:
+    """Create a connection to a local Elasticsearch instance and check that it's working"""
+    es_instance = Elasticsearch(
+        hosts=[{"host": "localhost", "port": port}],
+        use_ssl=True,
+        verify_certs=False,
+        index=index,
+    )
+
+    try:
+        es_instance.info()
+    except elasticsearch.exceptions.ConnectionError:
+        print(f"Could not connect to ES at localhost:{port} - are you port forwarding?")
+
+    if index is not None and not es_instance.indices.exists(index):
+        valid_indices = es_instance.indices.get_alias(index="*").keys()
+        raise ValueError(
+            f"Index {index} does not exist. Valid indices are {valid_indices}"
+        )
+
+    return es_instance
+
+
 class LocalElasticsearchClient:
     def __init__(self, index: str | None, port: int = 3333):
         """
@@ -14,16 +38,12 @@ class LocalElasticsearchClient:
         :param index: Index to connect to
         """
         self.port = port
-        self.es_instance = Elasticsearch(hosts=[{'host': 'localhost', 'port': port}], use_ssl=True, verify_certs=False, index=index)
-        self._check_connection()
+        self.es_instance = create_client(index, port)
         self._index = index
 
         if not index:
             return
 
-        if not self.es_instance.indices.exists(index):
-            raise ValueError(f"Index {index} does not exist. Valid indices are {self.list_all_indices()}")
-        
     def get(self, doc_id: str) -> dict:
         """Return a document in an index by its ID"""
         return self.es_instance.get(index=self._index, id=doc_id)
@@ -31,8 +51,10 @@ class LocalElasticsearchClient:
     def query(self, query: dict, size: int = 1000) -> list:
         """Return all documents in an index matching a query"""
         query = {"query": query}
-        res = self.es_instance.search(index=self._index, body=query, size=size)
-        print(f"Found {len(res['hits']['hits'])} documents in {self._index} out of {res['hits']['total']['value']}")
+        res = self.es_instance.search(index=self._index, body=query)
+        print(
+            f"Found {len(res['hits']['hits'])} documents in {self._index} out of {res['hits']['total']['value']}"
+        )
         return res["hits"]["hits"]
 
     def index(self, doc_id: str, body: dict):
@@ -65,23 +87,19 @@ class LocalElasticsearchClient:
         }
         return self.query(query, size)
 
-    def set_attribute(self, doc_id: str, attribute: str, value: str | dict[str,str]):
+    def set_attribute(self, doc_id: str, attribute: str, value: str | dict[str, str]):
         """Set an attribute for a document in an index - use a dict for nested properties"""
-        self.es_instance.update(index=self._index, id=doc_id, body={"doc": {attribute: value}})
+        self.es_instance.update(
+            index=self._index, id=doc_id, body={"doc": {attribute: value}}
+        )
 
-    def delete_attribute(self, index: str, doc_id: str, attribute: str):
+    def delete_attribute(self, doc_id: str, attribute: str):
         """Delete an attribute for a document in an index"""
-        self.es_instance.update(index=self._index, id=doc_id, body={"script": f"ctx._source.remove('{attribute}')"})
-
-    def list_all_indices(self) -> list[str]:
-        """Return all indices in the Elasticsearch instance"""
-        return list(self.es_instance.indices.get_alias(index="*").keys())
-
-    def _check_connection(self):
-        try:
-            self.es_instance.info()
-        except elasticsearch.exceptions.ConnectionError:
-            print(f"Could not connect to ES at localhost:{self.port} - are you port forwarding?")
+        self.es_instance.update(
+            index=self._index,
+            id=doc_id,
+            body={"script": f"ctx._source.remove('{attribute}')"},
+        )
 
     def create_index(self, new_index_name):
         """Create a new index"""
