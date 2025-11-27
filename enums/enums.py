ab6929e11f14c574567cf22d0b307523f103d3cf
Adam <74552077+adamtry@users.noreply.github.com>
2025-11-27 12:23:34 +0000

    Copy ssm parameters between accounts script (#46)
    * Add script to copy SSM parameters between AWS accounts and update enums for disaster recovery stage
    
    * Update usage instructions in copy_ssm_parameters.py for clarity

--- enums/enums.py
+++ enums/enums.py
@@ -1,4 +1,7 @@
 from enum import Enum
+from typing import Literal, cast
+
+Environment = Literal["development", "staging", "production"]
 
 
 class Stage(Enum):
@@ -13,14 +16,15 @@ class Stage(Enum):
     DEVELOPMENT_APIS = "development-apis"
     STAGING_APIS = "staging-apis"
     PRODUCTION_APIS = "production-apis"
+    DISASTER_RECOVERY = "disaster-recovery"
 
     @property
-    def env_name(self) -> str:
+    def env_name(self) -> Environment:
         value = self.value
         for stage_str in ["development", "staging", "production"]:
             if stage_str in value:
-                return stage_str
+                return cast(Environment, stage_str)
         raise ValueError(f"Stage {self.value} not recognised")
 
-    def to_env_name(self) -> str:
+    def to_env_name(self) -> Environment:
         return self.env_name
